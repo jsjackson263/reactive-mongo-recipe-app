59a0a51d3b9d460b83312f6af50afce070e0f4e8
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-02-18 01:51:37 +1300

    add RecipeController

---
+++ src/main/java/info/jsjackson/controllers/RecipeController.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package info.jsjackson.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import info.jsjackson.domain.Recipe;
+import info.jsjackson.services.RecipeService;
+
+/**
+ * @author josan
+ *
+ */
+@Controller
+public class RecipeController {
+	
+	RecipeService recipeService;
+
+	public RecipeController(RecipeService recipeService) {
+		this.recipeService = recipeService;
+	}
+	
+	
+	@RequestMapping("/recipe/show/{id}")
+	public String showById(@PathVariable String id, Model model) {
+		
+		Recipe recipe = recipeService.getById(new Long(id));
+		model.addAttribute("recipe", recipe);
+		
+		//return view name
+		return "recipe/show";
+	}
+	
+
+}
