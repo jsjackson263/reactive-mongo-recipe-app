f1184c57b930b7b9b710e4f9a484ca5dac2b048e
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-02-22 22:12:51 +1300

    re-organise the interface methods, & add saveRecipeCommand method

--- src/main/java/info/jsjackson/services/RecipeService.java
+++ src/main/java/info/jsjackson/services/RecipeService.java
@@ -2,9 +2,15 @@ package info.jsjackson.services;
 
 import java.util.Set;
 
+import info.jsjackson.commands.RecipeCommand;
 import info.jsjackson.domain.Recipe;
 
-public interface RecipeService extends CRUDService<Recipe> {
+public interface RecipeService {
 
-	Set<Recipe> getRecipes();
+	 Set<Recipe> getRecipes();
+
+	 Recipe findById(Long l);
+	
+	RecipeCommand saveRecipeCommand(RecipeCommand command);
+	
 }
