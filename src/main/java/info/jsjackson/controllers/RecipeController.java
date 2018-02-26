95ede61dd1e52a9397aa92206548bc29e1c41be5
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-02-26 21:30:35 +1300

    add  newRecipe and saveOrUpdate methods to RecipeController

--- src/main/java/info/jsjackson/controllers/RecipeController.java
+++ src/main/java/info/jsjackson/controllers/RecipeController.java
@@ -5,9 +5,13 @@ package info.jsjackson.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import info.jsjackson.commands.RecipeCommand;
 import info.jsjackson.domain.Recipe;
 import info.jsjackson.services.RecipeService;
 
@@ -25,7 +29,7 @@ public class RecipeController {
 	}
 	
 	
-	@RequestMapping("/recipe/show/{id}")
+	@RequestMapping("/recipe/{id}/show")
 	public String showById(@PathVariable String id, Model model) {
 		
 		Recipe recipe = recipeService.findById(new Long(id));
@@ -35,5 +39,28 @@ public class RecipeController {
 		return "recipe/show";
 	}
 	
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model) {
+		
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipe/recipeform";
+	}
 
+	@RequestMapping("/recipe/{id}/update")
+	public String updateRecipe(@PathVariable String id, Model model) {
+		
+		return "";
+	}
+	
+	//@RequestMapping(name = "recipe", method = RequestMethod.POST)  - alternatively
+	@PostMapping
+	@RequestMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		//@ModelAttribute tells Spring to bind the form post parameters to RecipeCommand object
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+		//tell SpringMVC to redirect to a specific url
+		return "redirect:/recipe/show/" + savedCommand.getId(); 
+		
+	}
 }
