416d1d9687398b9715076ccf0ef6e668cd718a08
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-03-18 14:13:45 +1300

    add 'showRecipeIngredient' to IngredientController

--- src/main/java/info/jsjackson/controllers/IngredientController.java
+++ src/main/java/info/jsjackson/controllers/IngredientController.java
@@ -3,37 +3,55 @@
  */
 package info.jsjackson.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import info.jsjackson.commands.IngredientCommand;
+import info.jsjackson.services.IngredientService;
 import info.jsjackson.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * @author josan 
+ * @author josan
  *
  */
 @Slf4j
 @Controller
 public class IngredientController {
-	
-	private final RecipeService recipeService;
 
-	public IngredientController(RecipeService recipeService) {
+	private final RecipeService recipeService;
+	private final IngredientService ingredientService;
+
+	public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
 		this.recipeService = recipeService;
+		this.ingredientService = ingredientService;
 	}
-	
+
 	@GetMapping
 	@RequestMapping("/recipe/{recipeId}/ingredients")
 	public String listIngredients(@PathVariable String recipeId, Model model) {
-		
+
 		log.debug("Getting ingredient list for recipe id:" + recipeId);
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 		return "recipe/ingredient/list";
 	}
-	
+
+	@GetMapping
+	@RequestMapping("/recipe/{recipeId}/ingredient/{ingredientId}/show")
+	public String showRecipeIngredient(@PathVariable String recipeId, @PathVariable String ingredientId, Model model) {
+		
+		IngredientCommand ingredientCommand = 
+				ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(ingredientId));
+		
+		model.addAttribute("ingredient", ingredientCommand);
+		
+		return "recipe/ingredient/show";
+
+	}
 
 }
