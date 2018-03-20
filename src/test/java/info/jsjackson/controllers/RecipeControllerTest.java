c9908439917f49cc7f88d48939949a3d56db0546
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-03-20 22:27:41 +1300

    organize imports

--- src/test/java/info/jsjackson/controllers/RecipeControllerTest.java
+++ src/test/java/info/jsjackson/controllers/RecipeControllerTest.java
@@ -1,14 +1,15 @@
 package info.jsjackson.controllers;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import java.util.Optional;
 
 import org.junit.Before;
 import org.junit.Test;
