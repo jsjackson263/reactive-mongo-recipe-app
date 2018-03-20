92d5dbd19294ff977d459672694a165ba41b4a83
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-03-20 23:21:22 +1300

    organize imports

--- src/main/java/info/jsjackson/controllers/IndexController.java
+++ src/main/java/info/jsjackson/controllers/IndexController.java
@@ -3,18 +3,13 @@
  */
 package info.jsjackson.controllers;
 
-import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import info.jsjackson.domain.Category;
 import info.jsjackson.domain.Recipe;
-import info.jsjackson.domain.UnitOfMeasure;
-import info.jsjackson.repositories.CategoryRepository;
-import info.jsjackson.repositories.UnitOfMeasureRepository;
 import info.jsjackson.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
