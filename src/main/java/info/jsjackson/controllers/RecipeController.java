8c71bc1d882d4c9704d754795fc7de55f49d5be6
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-03-24 23:49:13 +1300

    add exception handler method 'handleNotFound' that returns a 404  error to the browser

--- src/main/java/info/jsjackson/controllers/RecipeController.java
+++ src/main/java/info/jsjackson/controllers/RecipeController.java
@@ -3,24 +3,31 @@
  */
 package info.jsjackson.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import info.jsjackson.commands.RecipeCommand;
 import info.jsjackson.domain.Recipe;
+import info.jsjackson.exceptions.NotFoundException;
 import info.jsjackson.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author josan
  *
  */
+@Slf4j
 @Controller
 public class RecipeController {
 	
@@ -67,8 +74,23 @@ public class RecipeController {
 	@GetMapping("recipe/{id}/delete")
 	public String deleteRecipeById(@PathVariable String id) {
 		
+		log.debug("Deleting id: " + id);
+		
 		recipeService.deleteById(Long.valueOf(id));
 		return "redirect:/";
+		
+	}
+	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound() {
+		
+		log.error("Handling Not Found Exception");
+		
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("404error");
+		
+		return modelAndView;
 		
 	}
 	
