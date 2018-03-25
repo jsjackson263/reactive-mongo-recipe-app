4040707745528827e32a42cd914e90188508d3a0
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-03-25 22:18:04 +1300

    add validation annotations to RecipeCommand

--- src/main/java/info/jsjackson/commands/RecipeCommand.java
+++ src/main/java/info/jsjackson/commands/RecipeCommand.java
@@ -6,6 +6,13 @@ package info.jsjackson.commands;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
 import info.jsjackson.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,13 +28,31 @@ import lombok.Setter;
 public class RecipeCommand {
 
 	private Long id;
+	
+	@NotBlank
+	@Size(min = 3, max = 255)
 	private String description;
+	
+	@Min(1)
+	@Max(999)
 	private Integer prepTime;
+	
+	@Min(1)
+	@Max(999)
 	private Integer cookTime;
+	
+	@Min(1)
+	@Max(100)
 	private Integer servings;
+	
 	private String source;
+	
+	@URL
 	private String url;
+	
+	@NotBlank
 	private String directions;
+	
 	private Set<IngredientCommand> ingredients = new HashSet<>();
 	private Byte[] image;
 	private Difficulty difficulty;
