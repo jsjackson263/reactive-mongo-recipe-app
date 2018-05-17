02420f832ff84c754a27b8225365902620e5f9e7
jsjackson263 <jsjackson263@gmail.com>
2018-05-17 23:16:19 +1200

    MongoDB conversion: remove JPA annotations from domain objects, change id datatype to String

--- src/main/java/info/jsjackson/domain/Ingredient.java
+++ src/main/java/info/jsjackson/domain/Ingredient.java
@@ -5,15 +5,6 @@ package info.jsjackson.domain;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,21 +14,15 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@EqualsAndHashCode(exclude = {"recipe"})
-@Entity
 public class Ingredient {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private String id;
 	
 	private String description;
 	private BigDecimal amount;
 	
-	@OneToOne(fetch = FetchType.EAGER)
 	private UnitOfMeasure uom;
 	
-	@ManyToOne
 	private Recipe recipe;
 
 	 public Ingredient() {
