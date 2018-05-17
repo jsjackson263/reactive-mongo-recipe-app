81122701ec35f010e4c52f8fbf95dbe9e7458d5f
jsjackson263 <jsjackson263@gmail.com>
2018-05-17 23:17:20 +1200

    MongoDB conversion: remove JPA annotations from domain objects, change id datatype to String

--- src/main/java/info/jsjackson/domain/UnitOfMeasure.java
+++ src/main/java/info/jsjackson/domain/UnitOfMeasure.java
@@ -3,12 +3,6 @@
  */
 package info.jsjackson.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,31 +12,11 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-@Entity
 public class UnitOfMeasure {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private String id;
 	
 	private String description;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	
 	
 }
