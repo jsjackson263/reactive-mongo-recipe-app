57d2acbd902996d58e190be000aef8c82fa23a2c
jsjackson263 <jsjackson263@gmail.com>
2018-05-17 23:17:01 +1200

    MongoDB conversion: remove JPA annotations from domain objects, change id datatype to String

--- src/main/java/info/jsjackson/domain/Recipe.java
+++ src/main/java/info/jsjackson/domain/Recipe.java
@@ -6,23 +6,18 @@ package info.jsjackson.domain;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.*;
-
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author josan
  *
  */
-@Data
-@EqualsAndHashCode(exclude = {"recipes"})
-@Entity
+@Getter
+@Setter
 public class Recipe {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private String id;
 	
 	private String description;
 	private Integer prepTime;
@@ -30,30 +25,14 @@ public class Recipe {
 	private Integer servings;
 	private String source;
 	private String url;
-	
-	@Lob
 	private String directions;
-	
-	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients = new HashSet<>();
-	
-	//use the wrapper object - recommended by Hibernate team - do not use the primitive byte array
-	@Lob
 	private Byte[] image;
-	
-	@Enumerated(value = EnumType.STRING)
 	private Difficulty difficulty;
-	
-	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 	
-	@ManyToMany
-	@JoinTable(name = "recipe_category", 
-	joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
-	
-	//XXX:Document some of these setters (see pro-tips: - using setters for JPA bidirectional relationships)
 	public void setNotes(Notes notes) {
 		if (notes != null) {
 			this.notes = notes;
