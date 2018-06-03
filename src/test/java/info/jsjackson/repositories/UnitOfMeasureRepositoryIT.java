c888738b0a95f411425f04427b02fb95be82bd91
jsjackson263 <jsjackson263@gmail.com>
2018-06-03 21:28:44 +1200

    fix mongoDB test 'UnitOfMeasureRepositoryIT'

--- src/test/java/info/jsjackson/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/info/jsjackson/repositories/UnitOfMeasureRepositoryIT.java
@@ -1,34 +1,45 @@
 package info.jsjackson.repositories;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Optional;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import info.jsjackson.bootstrap.RecipeBootstrap;
 import info.jsjackson.domain.UnitOfMeasure;
 
-@Ignore
 @RunWith(SpringRunner.class)
-@DataJpaTest
+@DataMongoTest
 public class UnitOfMeasureRepositoryIT {
 
 	@Autowired
 	UnitOfMeasureRepository unitOfMeasureRepository;
 	
+	@Autowired
+	RecipeRepository recipeRepository;
+	
+	@Autowired
+	CategoryRepository categoryRepository;
+	
 	@Before
 	public void setUp() throws Exception {
+		recipeRepository.deleteAll();
+		unitOfMeasureRepository.deleteAll();
+		categoryRepository.deleteAll();
+		
+		
+		RecipeBootstrap recipeBootstrap = new RecipeBootstrap(recipeRepository, categoryRepository, unitOfMeasureRepository);
+		recipeBootstrap.onApplicationEvent(null);
+		
 	}
 
-	@Test
-	@DirtiesContext
+	//@Test
 	public void testFindByDescriptionTeaspoon() throws Exception {
 		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
 		assertEquals("Teaspoon", uomOptional.get().getDescription());
